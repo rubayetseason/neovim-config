268074540b50227c66d40a4d226670320df6f36f
rubayet-islam <season.ss2001@gmail.com>
2024-06-03 18:33:22 +0600

    base neovim config

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,24 @@
+-- Customize Treesitter
+
+---@type LazySpec
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    opts.ignore_install = { "help" }
+    -- add more things to the ensure_installed table protecting against community packs modifying it
+    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+      "lua",
+      "vim",
+
+      "html",
+      "css",
+      "typescript",
+      "prisma",
+
+      "c",
+      "cpp",
+
+      "markdown",
+    })
+  end,
+}
